69b250920a4decedcf94bf5c5d27d459e3168197
Julius Platon <j.platon@tu-bs.de>
2023-01-21 15:08:43 +0100

    Add MonadBaseControl IO instance and related dependencies

--- lib/Robotics/NXT/Internals.hs
+++ lib/Robotics/NXT/Internals.hs
@@ -1,4 +1,9 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Robotics.NXT.Internals where
 
@@ -6,13 +11,15 @@ import Control.Monad.State
 import Data.Time.Clock.POSIX
 import Data.Typeable
 import System.Hardware.Serialport
+import Control.Monad.Base
 
 import Robotics.NXT.Externals
+import Control.Monad.Trans.Control
 
 {-|
 Monad which encompasses interface to the NXT brick.
 -}
-newtype NXT a = NXT (StateT NXTInternals IO a) deriving (Monad, Applicative, MonadIO, Functor, MonadFix, MonadFail) -- NXT monad
+newtype NXT a = NXT (StateT NXTInternals IO a) deriving (Monad, Applicative, MonadIO, Functor, MonadFix, MonadFail, MonadBase IO) -- NXT monad
 
 {-|
 A token used for exposed internal functions.
@@ -24,6 +31,16 @@ data NXTInternals = NXTInternals {
     sleeptime :: Maybe Duration, -- sleep time limit in seconds
     lastkeepalive :: Maybe POSIXTime -- last time keep alive has been sent
   } deriving (Typeable)
+
+
+{-|
+MonadBaseControl instance, useful for creating threads.
+-}
+instance MonadBaseControl IO NXT where
+  type StM NXT a = StM (StateT NXTInternals IO) a
+  liftBaseWith f = NXT $ liftBaseWith $ \s -> f (s . (\(NXT nxt) -> nxt))
+  restoreM = NXT . restoreM
+
 
 instance Show NXTInternals where
   show = show . typeOf
